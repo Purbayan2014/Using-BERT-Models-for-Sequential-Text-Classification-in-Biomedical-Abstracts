eb4837c8e439e35981ba15d1332b2ca2ce8ff286
purbayan <purbayan2014@gmail.com>
2022-09-26 10:24:35 +0530

    transformer bugs fixed

--- Torch_release_experiments/torch_models/tc_transformer_train.py
+++ Torch_release_experiments/torch_models/tc_transformer_train.py
@@ -7,29 +7,31 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-from Utility.voice_engine import vc_arch
-
-epochs = 10
-patience = 3
-dump_path = 'model_dumps/transformer.pt'
-
-res = {
-    'training-loss' : [],
-    'validation-loss' : [],
-    'training-acurracy': [],
-    'validation-accuracy' : []
-}
+# from voice_engine import vc_arch
 
 
 
 def training_engine_and_evaluation_architecture(model, tdloader, device, patience,optimizer, loss_cat, vdloader, scheduler):
     """
     Training engine architecture for torch transformer model
+    
     """
+    epochs = 10
+    patience = 3
+    dump_path = 'model_dumps/transformer.pt'
+
+    res = {
+        'training-loss' : [],
+        'validation-loss' : [],
+        'training-acurracy': [],
+        'validation-accuracy' : []
+    }
+
+    global best_validation_loss
     best_validation_loss = np.inf
     for ep in range(epochs):
         print(f'------------------Training in epoch {ep+1}--------------------------------->')
-        vc_arch(f'------------------Training in epoch {ep+1}--------------------------------->')
+        # vc_arch(f'------------------Training in epoch {ep+1}--------------------------------->')
         training_loss = 0
         validation_loss = 0
         training_accuracy = 0
@@ -43,6 +45,7 @@ def training_engine_and_evaluation_architecture(model, tdloader, device, patienc
             optimizer.zero_grad()
             ans = model.forward(_ins, ln_nums, total_lns)
             loss = loss_cat(ans, lbls)
+            loss.backward()
             optimizer.step()
             training_loss += loss.item()
             pred_class = torch.softmax(ans, dim=1).argmax(dim=1)
@@ -75,14 +78,14 @@ def training_engine_and_evaluation_architecture(model, tdloader, device, patienc
         if validation_loss < best_validation_loss :
             best_validation_loss =  validation_loss
             print('The model is being saved please wait')
-            vc_arch('The model is being saved please wait')
+            # vc_arch('The model is being saved please wait')
             torch.save(model.state_dict(), dump_path)
             _patience = patience
         else: 
             _patience -= 1
             if not _patience: 
                 print('Stopping early the limit for patience has been reached')
-                vc_arch('Stopping early the limit for patience has been reached')
+                # vc_arch('Stopping early the limit for patience has been reached')
                 break
 
         res['training-loss'].append(training_loss)
@@ -90,13 +93,13 @@ def training_engine_and_evaluation_architecture(model, tdloader, device, patienc
         res['training-acurracy'].append(training_accuracy)
         res['validation-accuracy'].append(validation_accuracy)
 
-        print(f"The learning rate used for this model : {optimizer.param_groups[0]['lr']:.2f}")
+        print(f"The learning rate used for this model : {optimizer.param_groups[0]['lr']:.2E}")
         # Logging the parameters
         print(
-            f"Training loss : {training_loss:,.3f},\t"
-            f"Training Accuracy : {training_accuracy:,.3f},\t"
-            f"Validation loss :  {validation_loss:,.3f}, \t"
-            f"Validation Accuracy : {validation_accuracy:,.3f}, \t"
+            f"Training loss : {training_loss:.6f},\t"
+            f"Training Accuracy : {training_accuracy:.2f},\t"
+            f"Validation loss :  {validation_loss:.6f}, \t"
+            f"Validation Accuracy : {validation_accuracy:.2f}, \t"
         )
 
 
